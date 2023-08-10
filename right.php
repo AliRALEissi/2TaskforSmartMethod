0b5c3a1d15a60bd4393409dddb075ad5a57e56c7
AliRALEissi <138800106+AliRALEissi@users.noreply.github.com>
2023-08-10 14:18:58 +0300

    Add files via upload

---
+++ right.php
@@ -0,0 +1,35 @@
+<?php
+// Define database connection details
+$servername = "localhost";
+$username = "root";
+$password = "";
+$dbname = "robot"; // database name
+// database table name is robot_retrev
+
+// Create database connection
+$conn = mysqli_connect($servername, $username, $password, $dbname);
+
+// Check if connection was successful
+if (!$conn) {
+    die("Connection failed: " . mysqli_connect_error());
+}
+
+// Prepare and execute SQL statement for right movement
+$stmt = $conn->prepare("INSERT INTO robot_retrev (direction) VALUES (?)");
+$stmt->bind_param("s", $direction);
+$direction = "R";
+$stmt->execute();
+
+// Check if row was inserted successfully
+if ($stmt->affected_rows > 0) {
+    echo "Right movement recorded successfully";
+} else {
+    echo "Error recording right movement: " . $stmt->error;
+}
+
+// Close statement
+$stmt->close();
+
+// Close database connection
+mysqli_close($conn);
+?>
